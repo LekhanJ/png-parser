b7ff7e4da1289e011f289bedee49bb763818835b
LekhanJ <jomiwalelekhan@gmail.com>
2026-06-17 03:16:16 +0530

    Add PNG signature verification

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"slices"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	data, err := os.ReadFile("frame_5700.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = verifySignature(data[:8])
+	if err != nil {
+		fmt.Println("Error occurred: ", err)
+	}
+}
+
+func verifySignature(data []byte) (bool, error) {
+	signature := []byte{137, 80, 78, 71, 13, 10, 26, 10}
+	if !slices.Equal(data, signature) {
+		return false, errors.New("Invalid Signature")
+	}
+	return true, nil
 }
